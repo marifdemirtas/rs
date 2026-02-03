80313a2094b8bdb6202e2a0c6f0bcfdbf3ea9aa9
marifdemirtas <mehmetarifdemirtas@gmail.com>
2026-02-03 19:46:05 +0000

    Modify Setup Scripts

--- add-books.sh
+++ add-books.sh
@@ -2,12 +2,12 @@
 COURSE=cs102seaborn
 docker compose run --rm rsmanage rsmanage addcourse --course-name $COURSE --basecourse $COURSE --login-required --institution UIUC
 docker compose run --rm rsmanage rsmanage build $COURSE
-CSV_FILE=/home/trails-admin/data/users.csv
+CSV_FILE=users.csv
 
-# Read the CSV file line by line, skipping the header
+# # Read the CSV file line by line, skipping the header
 tail -n +2 "$CSV_FILE" | while IFS=',' read -r VAR1 VAR2 VAR3 VAR4 VAR5 VAR6; do
     echo "Adding user: $VAR1"
     echo docker compose run --rm rsmanage rsmanage adduser --username "$VAR1" --password "$VAR5" --first_name "$VAR3" --last_name "$VAR4" --email "$VAR2" --course "$VAR6"
     docker compose run --rm -T rsmanage rsmanage adduser --username "$VAR1" --password "$VAR5" --first_name "$VAR3" --last_name "$VAR4" --email "$VAR2" --course "$VAR6"
 done
-docker compose run --rm rsmanage rsmanage addinstructor --username katcun --course $COURSE
+docker compose run --rm rsmanage rsmanage addinstructor --username test_instructor --course $COURSE
--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -4,5 +4,5 @@ docker compose run --rm rsmanage rsmanage initdb
 docker compose up -d
 
 docker exec -u root rs-jobe-1 sh -c "mkdir -p /home/jobe00/.cache /home/jobe00/.config && chmod -R 777 /home/jobe00/.cache /home/jobe00/.config"
-docker cp ./figshow rs-jobe-1:/tmp/figshow && \
-docker exec -it rs-jobe-1 /usr/bin/python3 -m pip install /tmp/figshow
+docker cp ./tutorial rs-jobe-1:/tmp/tutorial && \
+docker exec -it rs-jobe-1 /usr/bin/python3 -m pip install /tmp/tutorial
