81e285751416fc41d401bdd4fbd7cb8c85a2b90f
marifdemirtas <mehmetarifdemirtas@gmail.com>
2026-02-04 17:04:43 +0000

    Update the 'tutorial' package to handle new data, grid visualization, warnings'

--- tutorial/setup.py
+++ tutorial/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tutorial",
-    version="0.1.0",
+    version="0.2.1",
     packages=find_packages(),
     include_package_data=True,  # <--- CRITICAL: Tells pip to look at MANIFEST.in
     install_requires=[
--- tutorial/tutorial/main.py
+++ tutorial/tutorial/main.py
@@ -3,6 +3,12 @@ import sys
 import secrets, os
 import pandas as pd
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings("ignore")
+
 def show_file(filename=None):
     # Use the first command line argument if filename isn't passed directly
     if filename is None and len(sys.argv) > 1:
@@ -15,10 +21,32 @@ def show_file(filename=None):
     with open(filename, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
-    print("<start_figure>"+ encoded_string + "<end_figure>")
+    print("@start_figure@" + encoded_string + "@end_figure@")
 
 
-def display(figure):
+def display(object):
+    if isinstance(object, sns.FacetGrid):
+        figure = object._figure
+    elif isinstance(grid._figure, mpl.pyplot.Figure):
+        figure = object
+    else:
+        msg = '''
+        display() failed.
+        display requires return object of a plotting function. For example:
+
+            figure = sns.barplot(x=..., y=..., data=...)
+            display(figure)
+
+        OR
+
+            grid = sns.FacetGrid(...)
+            grid.map(...)
+            display(grid)
+    
+        '''
+        print(msg)
+        raise ValueError(f"Cannot display figure: {object}")
+
     filename = secrets.token_hex(4) + ".png"
     figure.get_figure().savefig(filename)
     show_file(filename)
@@ -37,12 +65,17 @@ def load_dataset(name):
         file_path = os.path.join(base_path, 'data', 'weather.csv')
         return pd.read_csv(file_path)
         
+    elif name == "weather_long":
+        file_path = os.path.join(base_path, 'data', 'weather_long.csv')
+        return pd.read_csv(file_path)
+        
     else:
         # Standard error message
         msg = '''
         load_dataset should be called in one of two ways:
         restaurants = load_dataset("restaurants")
         weather = load_dataset("weather")
+        weather_long = load_dataset("weather_long")
         '''
         print(msg)
         raise ValueError(f"Unknown dataset: {name}")
